780e265b04139fb22b2653d108aaf15db567bc39
FailureMaster <mike.macabulos@gmail.com>
2019-09-18 20:50:41 +0800

    Edited and made some test component inside a component, later on all component will be separated in each file.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+function TodoList(){
+  return(
+    <div className="App">
+      <ul>
+        <li>Wordpress Training</li>
+        <li>Sass Training</li>
+        <li>Laravel training</li>
+      </ul>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Will create To-Do List app here</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <TodoList/>
   );
 }
 
